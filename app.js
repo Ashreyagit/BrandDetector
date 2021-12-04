eee79d60e546483f3be09ff8f8a8ea824a3f3fa0
Ashreyagit <shreya.abhyankar@cumminscollege.in>
2021-12-04 21:10:24 +0530

    Added audio support and color scheme

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const upload = multer({
   },
 });
 
-
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('/', express.static(__dirname));
+app.use('/views', express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
+app.use('public', express.static(__dirname + '/'));
+app.use('/result', express.static(__dirname + '/'));
 
 
 //MS Specific
@@ -30,7 +36,7 @@ const ComputerVisionClient =
 const ApiKeyCredentials = require("@azure/ms-rest-js").ApiKeyCredentials;
 
 
-
+var speak="";
 require('dotenv').config();
 const cloudinary = require("cloudinary").v2;
 
@@ -59,7 +65,9 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-
+app.get("/result", (req, res) => {
+res.sendFile(path.join(__dirname, 'output.mp3'));
+});
 var text="";
 app.post("/", upload.single("file-to-upload"), async (req, res) => {
   try {
@@ -77,7 +85,11 @@ app.post("/", upload.single("file-to-upload"), async (req, res) => {
        console.log(`${brands.length} brand${brands.length != 1 ? 's' : ''} found:`);
        for (const brand of brands) {
          console.log(`    ${brand.name} (${brand.confidence.toFixed(2)} confidence)`);
+         
+        
        }
+       var speak=brands[0].name+"";
+       console.log(speak+"");
     text=" ";
         
       } else 
@@ -89,12 +101,88 @@ app.post("/", upload.single("file-to-upload"), async (req, res) => {
       }
      // </snippet_brands>
      console.log(text);
-    res.render("result.ejs", { brands: brands, img: brandURLImage ,msgg:text });
+
+
+ 
+     console.log('-------------------------------------------------');
+     console.log('DETECT COLOR SCHEME');
+     console.log();
+
+     // <snippet_colors>
+     const colorURLImage = result.secure_url;
+     console.log(colorURLImage);
+     // Analyze URL image
+     console.log('Analyzing image for color scheme...', colorURLImage.split('/').pop());
+     console.log();
+     const color = (await computerVisionClient.analyzeImage(colorURLImage, { visualFeatures: ['Color'] })).color;
+     printColorScheme(color);
+     // </snippet_colors>
+     var colorr="";
+     // <snippet_colors_print>
+     // Print a detected color scheme
+     function printColorScheme(colors) {
+       console.log(`Image is in ${colors.isBwImg ? 'black and white' : 'color'}`);
+       console.log(`Dominant colors: ${colors.dominantColors.join(', ')}`);
+       console.log(`Dominant foreground color: ${colors.dominantColorForeground}`);
+       console.log(`Dominant background color: ${colors.dominantColorBackground}`);
+       console.log(`Suggested accent color: #${colors.accentColor}`);
+
+
+      colorr =colors.dominantColorForeground;
+      console.log(colorr);
+     }
+     // </snippet_colors_print>
+     /**
+      * END - Detect Color Scheme
+      */
+     console.log();
+
+
+    res.render("result.ejs", { brands: brands, img: brandURLImage ,msgg:text,colorr:colorr });
   } catch (err) {
        
     console.log(err);
     
   }
+
+  const { SpeechSynthesisOutputFormat, SpeechConfig, AudioConfig, SpeechSynthesizer } = require("microsoft-cognitiveservices-speech-sdk");
+
+  function synthesizeSpeech() {   
+     
+    
+      //replace "YourSubscriptionKey" and "YourServiceRegion" with your azure key and region
+      //e.g. const speechConfig = SpeechConfig.fromSubscription("29f3f317abe376fd3b2a9b773112646d", "westeurope");
+      const speechConfig = SpeechConfig.fromSubscription("e8161a6f99e24aee8938266971c36ff1", "eastus");
+      speechConfig.speechSynthesisOutputFormat = SpeechSynthesisOutputFormat.Audio24Khz160KBitRateMonoMp3;
+      const audioConfig = AudioConfig.fromAudioFileOutput("output.mp3");
+  
+      const synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+      synthesizer.speakTextAsync(
+          speak,
+          result => {
+              if (result) {
+                  console.log(JSON.stringify(result));
+              }
+              synthesizer.close();
+          },
+          error => {
+              console.log(error);
+              synthesizer.close();
+          });
+
+          
+  }
+  
+  synthesizeSpeech();
+  
+
+ 
+
+      
+      
 });
+
+
+
 
 app.listen(process.env.PORT || 8000);
---
+++ public/main.js
@@ -0,0 +1,9 @@
+(function(){
+    // all your code here
+    
+    var audio=new Audio("output.mp3");
+    audio.load(); //call this to just preload the audio without playing
+    
+  audio.play();
+    // ...
+  })();
