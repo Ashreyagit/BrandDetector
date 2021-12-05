8b87948f57aaf636f7c4461efe267afe1fcffadb
Ashreyagit <shreya.abhyankar@cumminscollege.in>
2021-12-05 17:23:43 +0530

    d

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const multer = require("multer");
 const upload = multer({
   storage: multer.diskStorage({}),
